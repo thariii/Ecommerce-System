257d8c7ce1af1d54393671572bfbf1920015c638
Tharika Lakshani <Tharika.Lakshani@ATTUNE.AD>
2019-05-18 10:45:48 +0530

    First commit by tharika

---
+++ src/main/java/com/pafecom/products/PafecomApplication.java
@@ -0,0 +1,13 @@
+package com.pafecom.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafecomApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pafecom/springboot/restful/product/ProductRespository.java
@@ -0,0 +1,7 @@
+package com.pafecom.springboot.restful.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRespository extends JpaRepository<Product, Long>{
+
+}
