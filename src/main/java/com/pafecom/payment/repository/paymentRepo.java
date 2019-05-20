d57af0e9b8356c73048e122871408cbb1f77eb3a
dakshika225 <48018316+dakshika225@users.noreply.github.com>
2019-05-20 13:31:40 +0530

    Delete paymentRepo.java

--- src/main/java/com/pafecom/payment/repository/paymentRepo.java
+++
@@ -1,8 +0,0 @@
-package payment_project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.*;
-import payment_project.model.payment;
-public interface paymentRepo extends JpaRepository <payment, Long> {
-
-}
